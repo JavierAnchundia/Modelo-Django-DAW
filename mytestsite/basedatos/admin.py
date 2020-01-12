b47df16dafd2968834856913d476f68cd2f3d9b5
JavierJesus <jaanrosa@espol.edu.ec>
2020-01-12 15:08:24 -0500

    Proyecto de DAW, creacion de Base de Datos utilizando Django

---
+++ mytestsite/basedatos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Foto, Regla, Rol, Persona, Lugar, Noticias, Comentario
+
+# Register your models here.
+
+admin.site.register(Foto)
+admin.site.register(Regla)
+admin.site.register(Rol)
+admin.site.register(Persona)
+admin.site.register(Lugar)
+admin.site.register(Noticias)
+admin.site.register(Comentario)
+
